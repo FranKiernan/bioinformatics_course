3f73e1ef9eba1b4394988fbbd8dc6560e18a8aa7
fran.kiernan@kcl.ac.uk <fkiernan@ucl.ac.uk>
2019-03-20 14:47:36 +0000

    not tested: updated script to make empty dirs and README.md

--- scripts/my-script.sh
+++ scripts/my-script.sh
@@ -1,1 +1,9 @@
 #!/usr/bin/env bash
+# making empty directories in my git repo
+mkdir -p analysis docs data
+
+# add README.md to each directory
+for my_directory in scripts analysis docs data;do
+touch ${my_directory}/README.md
+echo "# ${my_directory}" >> ${my_directory}/README.md
+done
